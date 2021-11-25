edd0f39edc771da5187feb2f53934aaf71dbd082
Omar-Servidores <L19180552@slp.tecnm.mx>
2021-11-25 00:02:36 -0600

    Base de datos

---
+++ database/seeds/LibroSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\facades\DB;
+
+class LibroSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	DB::table('libro')->insert(['nombre'=>'Estructura de Datos','autor'=>'Luis Joyanes',]);
+    	DB::table('libro')->insert(['nombre'=>'Programacion orientada a objetos','autor'=>'Luis Joyanes',]);
+        //
+    		
+    }
+}
